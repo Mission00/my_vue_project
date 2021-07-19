5857ec9deb26bba9ff2f8d17d897aa024846da8b
Mission00 <1170485357@qq.com>
2021-07-19 22:27:56 +0800

    删了两换行

--- src/main.js
+++ src/main.js
@@ -2,17 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-
 import Video from 'video.js'
 import 'video.js/dist/video-js.css'
 
 Vue.prototype.$video = Video//播放器
 
-
 import ElementUI from 'element-ui' //element-ui的全部组件
 import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
 import 'element-ui/lib/theme-chalk/base.css';// fade/zoomxi
-
 
 Vue.use(ElementUI) //使用elementUI
 
